a28db1fa268019836a34c83c8a55ebf8f288636c
alvarobeltr <alvarobeltran6@gmail.com>
2025-03-01 13:31:28 +0100

    Update zlel_p1.py

--- zlel/zlel_p1.py
+++ zlel/zlel_p1.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 .. module:: zlel_main.py
-    :synopsis:
+    :synopsis: This module contains functions that are used in the main program
+    in order to parse the matrixes that will be used to solve the circuit.
 
 .. moduleauthor:: Eneko Aranburu (earanburu006@gmail.com) eta Alvaro Beltran (abeltrandenanc002@ikasle.ehu.eus)
 
@@ -38,7 +39,7 @@ def cir_parser(filename):
         cir = np.delete(cir, -1, axis=0)
     except ValueError:
         sys.exit("File corrupted: .cir size is incorrect.")
-
+    """
     # numpy usefull exmaples
     print("================ cir ==========")
     print(cir)
@@ -85,7 +86,7 @@ def cir_parser(filename):
     print("\n======== k[0] = \"987654321\" ==========")
     k[0] = "987654321"
     print(k)
-
+"""
     ''' https://www.geeksforgeeks.org/modify-numpy-array-to-store-an-arbitrary-length-string/
     The dtype of any numpy array containing string values is the maximum
     length of any string present in the array. Once set, it will only be able
@@ -105,10 +106,31 @@ def cir_parser(filename):
 
 
 def luzatu_cir(cir_el, cir_nd, cir_val, cir_ctr):
-    '''
-    Elementu guztiak zeharkatu ikusteko zeintzuk diren adar bi baino gehiago 
-    dituztenak.
-    '''
+    #Comprobarr returns
+    """
+    This function expands the matrixes we obtained in the function before
+    in order to fit with the number of the branches of the elements added.
+
+    Parameters
+    ----------
+    | cir_el: np array of strings with the elements to parse. size(1,b)
+    | cir_nd: np array with the nodes to the circuit. size(b,4)
+    | cir_val: np array with the values of the elements. size(b,3)
+    | cir_ctrl: np array of strings with the element which branch
+    | controls the controlled sources. size(1,b)
+
+    Returns
+    -------
+    cir_el2: np array of strings with the elements to parse.
+    size(b,n-1). cir_el extended
+    cir_nd2: np array with the nodes to the circuit. size(b,4).
+    cir_nd extended
+    cir_val2: np array with the values of the elements. size(b,3)
+    cir_val extended.
+    cir_ctrl2: np array of strings with the element which branch
+    controls the controlled sources. size(b,1). cir_ctrl extended.
+
+    """
     cir_el2 = []
     cir_nd2 = []
     cir_val2 = []
@@ -144,29 +166,100 @@ def luzatu_cir(cir_el, cir_nd, cir_val, cir_ctr):
 
 
 def getAdarrak(cir_el):
+    """
+    This function returns the size of the list of elements obtained from
+    cir_el.
+
+    Parameters
+    ----------
+    cir_el: np array of strings with the elements to parse. size(1,b)
+    
+    Returns
+    -------
+    b : an integer which represents the number of branches in the circuit.
+
+    """
     b = 0
     for s in cir_el:
         if s[0].lower() == "a" or s[0].lower() == "q":
-            b = b+2
+            b += 2
         else:
-            b = b+1
+            b += 1
     return b
 
 
 def getNodes(cir_nd):
+    """
+    This funcion takes the cir_nd matrix and returns a list made
+    up by the nodes of the circuit.
+
+    Parameters
+    ----------
+    cir_nd: np array with the nodes of the circuit. size(b,4)
+
+    Returns
+    -------
+    nodes : a sorted-in-ascending-order np array of the nodes
+
+    """
     nodes = np.unique(cir_nd)
     return np.sort(nodes)
 
 
 def getNodesNumber(cir_nd):
+    """
+    This funcion returns the size of the list of nodes from cir_nd using 
+    the getNodes function.
+
+    Parameters
+    ----------
+    cir_nd: np array with the nodes of the circuit. size(b,4)
+
+    Returns
+    -------
+    n : an integer which represents the number of nodes in the "nodes" list.
+
+    """
     return np.size(getNodes(cir_nd))
 
+'''Elementu kopurua (el_num)'''
+def getEl_num(cir_el):
+    """
+    This function returns the size of the list of elements obtained from
+    cir_el.
 
-def getElementNumber(cir_el):
+    Parameters
+    ----------
+    cir_el: np array of strings with the elements to parse.
+    size(el_num,n-1)
+
+    Returns
+    -------
+    el_num : an integer which represents the number of elements in the circuit.
+
+    """
     return np.size(cir_el)
 
 
 def getInzidentziaMatrix(n, b, cir_nd2):
+    """
+    This function return np array which represents the incident matrix
+    of the circuit.
+
+    Parameters
+    ----------
+    n : an integer which represents the number of nodes in the "nodes" list.
+
+    cir_nd2 : np array with the nodes to the circuit. size(b,4).
+    cir_nd extended
+
+    b : an integer which represents the number of branches in the circuit.
+
+    Returns
+    -------
+    Aa : a np array which represents the incident matrix of the circuit.
+
+    """
     Aa = np.zeros((n, b), dtype=int)  # Crear matriz de ceros de tamaño n x b
     nodes = getNodes(cir_nd2)  # Obtener nodos únicos
     for col, branch in enumerate(cir_nd2):  # Iterar sobre cada rama
@@ -181,23 +274,93 @@ def getInzidentziaMatrix(n, b, cir_nd2):
 
 
 def getMurriztutakoIntzidentziaMatrix(Aa, n):
+    """
+    This function return the matrix which will be used to solve the Tableau
+    equations.
+
+    Parameters
+    ----------
+    Aa : a np array which represents the incident matrix of the circuit.
+
+    n : an integer which represents the number of nodes in the "nodes" list.
+
+    Returns
+    -------
+    A : a np array wich represents the reduced matrix o i_mat.
+
+    """
     return np.array(Aa[1:n, :])
 
 
-def Erroreak1(nodes):
+def ErreferentziNodoa(nodes):
+    """
+    This function is used to detect if the reference-node is missing.
+
+    Parameters
+    ----------
+    nodes : a sorted-in-ascending-order np array of the nodes.
+
+    Returns
+    -------
+    None.
+
+    Rises
+    -------
+    SystemExit.
+
+    """
     if nodes[0] != 0:
         sys.exit("Erreferentzia nodoa falta da.")
 
 
-def Erroreak2(Aa, nodes):
+def KonexioBakarrekoNodoak(Aa, nodes):
+    """
+    This function is used to detect if any node is connected to nothing; floating.
+
+    Parameters
+    ----------
+    Aa : a np array which represents the incident matrix of the circuit.
+    
+    nodes : a sorted-in-ascending-order np array of the nodes.
+
+    Returns
+    -------
+    None.
+
+    Rises
+    -------
+    SystemExit.
+
+    """
     for n, x in enumerate(Aa):
         if np.size(np.flatnonzero(x != 0)) < 2:
             node = nodes[n]
             sys.exit(f"{node}. nodoa adar bakarrera konektatuta dago.")
 
 
+def TentsioIturriakParaleloan:(cir_el2, cir_val2, Aa):
+    """
+    This function is used to detect if Voltage sources are connected in parallel.
 
-def Erroreak3(cir_el2, cir_val2, Aa):
+    Parameters
+    ----------
+    Aa : a np array which represents the incident matrix of the circuit.
+    
+    cir_el2: np array of strings with the elements to parse.
+    size(b,n-1). cir_el extended
+    
+    cir_val2: np array with the values of the elements. size(b,3)
+    cir_val extended.
+
+    Returns
+    -------
+    None.
+
+    Rises
+    -------
+    SystemExit.
+
+    """
     """
     Tentsio iturri ezberdinak (tentsio desberdina dutenak) nodo berdinetara konektatuta
     badaude (paraleloan), errorea sortzen du. Gainera, tentsio berdina duten baina
@@ -221,7 +384,57 @@ def Erroreak3(cir_el2, cir_val2, Aa):
                 if not np.array_equal(Aa[:, adar1], Aa[:, adar2]):
                     sys.exit(f"Errorea: {cir_el2[adar1]} eta {cir_el2[adar2]} tentsio berdinekin paraleloan baina norantza kontrakoan daude.")
 
+def KorronteIturriakSeriean(elementos_circuito, nodos_circuito, valores_circuito, matriz_corriente, num_ramas):
+    elementos = {}
+    nodos_problema = set()
 
+    for indice, elemento in enumerate(elementos_circuito):
+        if elemento[0].lower() in ("i", "g", "y"):
+            elementos[indice] = {"nodos": nodos_circuito[indice], "tipo": elemento[0].lower(), "valor": valores_circuito[indice][0]}
+
+    for indice_x, datos_x in elementos.items():
+        for indice_y, datos_y in elementos.items():
+            if indice_x != indice_y:
+                nodos_x = datos_x["nodos"]
+                nodos_y = datos_y["nodos"]
+                if (nodos_x[0] == nodos_y[0] and nodos_x[1] != nodos_y[1]) or \
+                   (nodos_x[0] == nodos_y[1] and nodos_x[1] != nodos_y[0]) or \
+                   (nodos_x[1] == nodos_y[0] and nodos_x[0] != nodos_y[1]) or \
+                   (nodos_x[1] == nodos_y[1] and nodos_x[0] != nodos_y[0]):
+                    nodos_problema.add((nodos_x[0],nodos_x[1]))
+                    nodos_problema.add((nodos_y[0],nodos_y[1]))
+
+    for nodo in nodos_problema:
+        rama_lista = obtener_ramas(nodo[0], elementos_circuito, nodos_circuito)
+        if all(elemento[0].lower() in ("i", "g", "y") for elemento in rama_lista):
+            suma_corriente = sum(map(lambda i: valores_circuito[i][0] * matriz_corriente[nodo[0]][i], range(num_ramas)))
+            if suma_corriente != 0:
+                print(f"Fuentes de corriente en serie en el nodo {nodo}.")
+
+def obtener_ramas(nodo, cir_el2, cir_nd2):
+    """
+    This function returns a list of the branches connected to a node
+    in the circuit.
+
+    Parameters
+    ----------
+    nodoa : an integer which represents a node in the circuit
+    cir_el_luz: np array of strings with the elements to parse.
+    size(b,n-1). cir_el extended
+    cir_nd_luz: np array with the nodes to the circuit. size(b,4).
+    cir_nd extended
+
+    Returns
+    -------
+    lista : a list of the branches connected to a node in the circuit.
+
+    """
+    lista = []
+    for i in range(0, np.size(cir_el_luz)):
+        for x in cir_nd_luz[i]:
+            if x == nodoa:
+                lista.append(cir_el_luz[i])
+    return lista
 def print_cir_info(cir_el, cir_nd, b, n, nodes, el_num):
     """ Prints the info of the circuit:
         |     1.- Elements info
