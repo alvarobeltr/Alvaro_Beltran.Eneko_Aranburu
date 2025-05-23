2389b800b184cc7fd49cc8baf6aca4048d9f9e4b
Enekoanbu <128797340+Enekoanbu@users.noreply.github.com>
2025-05-23 13:27:30 +0200

    a

--- zlel/zlel_p4.py
+++ zlel/zlel_p4.py
@@ -12,6 +12,7 @@ import math
 import time
 import numpy as np
 import sys
+
 if __name__ == "zlel.zlel_p4":
     import zlel.zlel_p1 as zl1
     import zlel.zlel_p2 as zl2
@@ -20,7 +21,8 @@ else:
     import zlel_p1 as zl1
     import zlel_p2 as zl2
     import zlel_p3 as zl3
-    
+
+
 def dynamic(circuit):
     """
         This function takes a cir_parser2 and returns whether there are
@@ -37,20 +39,19 @@ def dynamic(circuit):
     """
     d = False
     d_el = []
-    k = 0
-    for el in circuit[0]:
-        if el[0][0] == "C": ###
+    for k, el in enumerate(circuit[0]):
+        if el[0][0] == "C":
             d = True
             pos = ("C", k)
             d_el.append(pos)
-        elif el[0][0] == "L": ##
+        elif el[0][0] == "L":
             d = True
             pos = ("L", k)
             d_el.append(pos)
-        k += 1
     return [d, d_el]
 
-def Euler_BackWard(A, circuit, elements, h, t, pre_sol):
+
+def Euler_BackWard(A, circuit, MNUs, h, t, pre_sol):
     """
 
         This function takes a cir_parser2 and elements, and in case there is a
@@ -70,32 +71,31 @@ def Euler_BackWard(A, circuit, elements, h, t, pre_sol):
     d = dynamic(circuit)
     if d[0]:
         d_el = d[1]
-        M = elements[0]
-        N = elements[1]
-        u = elements[2]
-        v = circuit[2]
-        # Ai = zl1.inc_matrix(cir_parser2)
+        M = MNUs[0]
+        N = MNUs[1]
+        Us = MNUs[2]
+        cir_val = circuit[2]
         n = len(A)
         b = len(A[0])
         if t == 0:
             for el, k in d_el:
                 if el == "C":
                     M[k][k] = 1
-                    N[k][k] = (-1)*h/v[k][0]
-                    u[k] = v[k][1]
+                    N[k][k] = (-1)*h/cir_val[k][0]
+                    Us[k] = cir_val[k][1]
                 else:
-                    M[k][k] = (-1)*h/v[k][0]
+                    M[k][k] = (-1)*h/cir_val[k][0]
                     N[k][k] = 1
-                    u[k] = v[k][1]
+                    Us[k] = cir_val[k][1]
         else:
             for el, k in d_el:
                 if el == "C":
                     vc = pre_sol[n-1+k]
-                    u[k] = vc
+                    Us[k] = vc
                 else:
                     ic = pre_sol[n-1+b+k]
-                    u[k] = ic
-        elements = [M, N, u]
+                    Us[k] = ic
+        return [M, N, Us]
 
 def save_as_csv_tr(b, n, filename, MNUs, circuit, start, end, step, operation):
     """ This function generates a csv file with the name filename.
@@ -121,8 +121,6 @@ def save_as_csv_tr(b, n, filename, MNUs, circuit, start, end, step, operation):
     pre_sol = []
     with open(filename, 'w') as file:
         print(header, file=file)
-        # Get the indices of the elements corresponding to the sources.
-        # The freq parameter cannot be 0 this is why we choose cir_tr[0].
         t = start
         t1=0
         while t <= end:
@@ -131,23 +129,42 @@ def save_as_csv_tr(b, n, filename, MNUs, circuit, start, end, step, operation):
                     w = 2*math.pi*cir_val[k][1]
                     MNUs[2][k] = cir_val[k][0]*math.sin((w*t) +
                                                    (math.pi*cir_val[k][2]/180))
-            Euler_BackWard(circuit, MNUs, tr[2], t1, pre_sol)
+            MNUs = Euler_BackWard(A, circuit, MNUs, tr[2], t1, pre_sol)
             zl3.NR(A, circuit, MNUs)
             sol = zl2.Tableau(A, MNUs[0], MNUs[1], MNUs[2])
             pre_sol = sol
-            # Inserte the time
             sol = np.insert(sol, 0, t)
-            # sol to csv
             sol_csv = ','.join(['%.9f' % num for num in sol])
             print(sol_csv, file=file)
-            t = round(t + step, 10)  # 10 decimals to avoid precision errors
-            t1+=1
+            t = round(t + step, 10)
+            t1 += 1
+
+
+def prepare_dynamic_OP(circuit, MNUs):
+    cir_el = circuit[0]
+    M, N, U = MNUs
+    for k, el in enumerate(cir_el):
+        tipo = el[0][0]
+        if tipo == "C":
+            # Capacitor abierto: corriente = 0
+            M[k] = [0]*len(M[k])
+            N[k] = [0]*len(N[k])
+            N[k][k] = 1
+            U[k] = 0
+        elif tipo == "L":
+            # Inductor cortocircuitado: voltaje = 0
+            M[k] = [0]*len(M[k])
+            N[k] = [0]*len(N[k])
+            M[k][k] = 1
+            U[k] = 0
+    return [M, N, U]
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     else:
-        filename = "../cirs/all/3_zlel_arteztailea.cir"
+        filename = "../cirs/all/3_zlel_RLC.cir"
     
     cp = zl2.cir_parser(filename)
     circuit = zl2.luzatu_cir(cp)
@@ -164,6 +181,7 @@ if __name__ == "__main__":
     MNUs = zl2.getMNUs(circuit)
     Aa = zl1.getInzidentziaMatrix(n, b, circuit[1])
     A = zl1.getMurriztutakoIntzidentziaMatrix(Aa, n)
+    MNUs = prepare_dynamic_OP(circuit, MNUs)
     zl3.NR(A, circuit, MNUs)
     # Verificar qué simulaciones ejecutar
     if op[".OP"]:
